a2b54098749c541335b8c29e81352e1beb887f2f
Konstantin Startsev <kstartsev@gmail.com>
2016-09-16 12:25:34 +0700

    +

---
+++ 004/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Euler
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var max = 0;
+
+            for (var i = 999; i >= 100; i--)
+            {
+                for (var j = 999; j >= 100; j--)
+                {
+                    var tmp = i*j;
+                    if (tmp < max)
+                    {
+                        break;
+                    }
+
+                    var str = tmp.ToString();
+                    var reversed = new string(str.Reverse().ToArray());
+                    if (str == reversed)
+                    {
+                        max = Math.Max(max, tmp);
+                    }
+                }
+            }
+
+            Console.Out.WriteLine(max);
+            Console.In.ReadLine();
+        }
+    }
+}
---
+++ 005/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Euler
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var max = 20;
+            var factors = new List<int>();
+            
+            for (var i = max; i >= 1; i--)
+            {
+                var list = new List<int>();
+                var tmp = i;
+                var div = 2;
+                while (tmp > 1)
+                {
+                    if ((tmp % div) == 0)
+                    {
+                        list.Add(div);
+                        tmp = tmp / div;
+                    }
+                    else
+                    {
+                        div = div + 1;
+                    }
+                }
+
+                foreach (var factor in factors)
+                {
+                    var index = list.IndexOf(factor);
+                    if (index != -1)
+                    {
+                        list.RemoveAt(index);
+                    }
+                }
+
+                factors = factors.Concat(list).ToList();
+            }
+
+            var x = factors.Aggregate(1, (res, fact) => res * fact);
+            Console.Out.WriteLine(string.Join(",", factors));
+            Console.Out.WriteLine(x);
+            Console.In.ReadLine();
+        }
+    }
+}
