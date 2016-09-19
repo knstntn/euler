60f160d7b55cbf06651d87a232180360a5f36ca4
Konstantin Startsev <kstartsev@gmail.com>
2016-09-19 12:27:48 +0700

    +

---
+++ 006/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Euler
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            const int max = 100;
+            var sum = 0;
+
+            for (var i = 1; i <= max; i++)
+            {
+                for (var j = 1; j <= max; j++)
+                {
+                    if (i == j) continue;
+
+                    sum += i*j;
+                }
+            }
+
+            Console.Out.WriteLine(sum);
+            Console.In.ReadLine();
+        }
+    }
+}
---
+++ 007/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Euler
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var index = 1;
+            var num = 2;
+
+            while (index < 10001)
+            {
+                num++;
+                if (IsPrime(num))
+                {
+                    index += 1;
+                }
+            }
+
+            Console.Out.WriteLine(num);
+            Console.In.ReadLine();
+        }
+
+        private static bool IsPrime(long n)
+        {
+            if ((n%2) == 0)
+            {
+                return false;
+            }
+            var sqrt = Math.Sqrt(n);
+            for (long i = 3; i <= sqrt; i++)
+            {
+                if ((n%i) == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
