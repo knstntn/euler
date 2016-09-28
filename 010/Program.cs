8b21fce33a20f2911520b2141e7f3ca41870f793
Konstantin Startsev <kstartsev@gmail.com>
2016-09-28 12:16:01 +0700

    +

---
+++ 010/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Euler
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            const int max = 2000000;
+
+            var sieve = Enumerable.Range(2, max - 2).ToDictionary(x => x, x => true);
+            var next = sieve.First();
+
+            while (Math.Pow(next.Key, 2) < max)
+            {
+                var remove = sieve.Where(x => x.Value
+                                              && Math.Pow(next.Key, 2) <= x.Key
+                                              && (x.Key%next.Key) == 0).Select(x => x.Key).ToArray();
+                foreach (var key in remove)
+                {
+                    sieve.Remove(key);
+                }
+
+                next = sieve.First(x => x.Key > next.Key);
+            }
+            var sum = sieve.Sum(x => (long)x.Key);
+
+            Console.Out.WriteLine(sum);
+            Console.In.ReadLine();
+        }
+    }
+}
