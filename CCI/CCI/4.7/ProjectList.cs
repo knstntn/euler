f0379f4eac709b3ab8606f0259c2b8d52fda874c
Konstantin Startsev <kstartsev@gmail.com>
2016-12-20 11:53:55 +0700

    +

---
+++ CCI/CCI/4.7/ProjectList.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace CCI
+{
+    public class ProjectList
+    {
+        private static readonly Queue<Vertex> sorted = new Queue<Vertex>();
+
+        // there cannot be cycles
+        public static IEnumerable<string> Schedule(string[] projects, IEnumerable<Tuple<string, string>> dependencies)
+        {
+            var g = new Vertex[projects.Length];
+            for (var i = 0; i < projects.Length; i++)
+            {
+                g[i] = new Vertex(projects[i]);
+            }
+
+            foreach (var dep in dependencies)
+            {
+                var v = g.Single(x => x.Value == dep.Item1);
+                var w = g.Single(x => x.Value == dep.Item2);
+
+                v.Children.Add(w);
+            }
+
+            dfs(g);
+
+            while (sorted.Count > 0)
+            {
+                var v = sorted.Dequeue();
+                yield return v.Value;
+            }
+        }
+
+        private static void dfs(Vertex[] g)
+        {
+            foreach (var v in g)
+            {
+                if (v.Visited)
+                {
+                    continue;
+                }
+                dfs(v);
+            }
+        }
+
+        private static void dfs(Vertex v)
+        {
+            v.Visited = true;
+
+            foreach (var vertex in v.Children)
+            {
+                if (!vertex.Visited)
+                {
+                    dfs(vertex);
+                }
+            }
+
+            sorted.Enqueue(v);
+        }
+
+        private class Vertex
+        {
+            public Vertex(string value)
+            {
+                Value = value;
+                Visited = false;
+                Children = new List<Vertex>();
+            }
+
+            public string Value { get; }
+            public bool Visited { get; set; }
+            public IList<Vertex> Children { get; }
+        }
+
+        [TestFixture]
+        public class ProjectListTests
+        {
+            [Test]
+            public void Test()
+            {
+                var projects = new[] {"a", "b", "c", "d", "e", "f"};
+                var deps = new[]
+                {
+                    Tuple.Create(projects[3], projects[0]),
+                    Tuple.Create(projects[1], projects[5]),
+                    Tuple.Create(projects[3], projects[1]),
+                    Tuple.Create(projects[0], projects[5]),
+                    Tuple.Create(projects[2], projects[3])
+                };
+                var res = Schedule(projects, deps).ToList();
+                CollectionAssert.AreEqual(new[] {"f", "a", "b", "d", "c", "e"}, res);
+            }
+        }
+    }
+}
---
+++ CCI/CCI/4.9/TreePermutations.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace CCI
+{
+    public class TreePermutations
+    {
+        public static IEnumerable<List<int>> ListArrays(TreeNode root)
+        {
+            if (root == null)
+            {
+                return new List<List<int>> {new List<int>()};
+            }
+
+            var left = ListArrays(root.Left);
+            var right = ListArrays(root.Right);
+
+            return Weave(root.Value, left, right).ToArray();
+        }
+
+        private static IEnumerable<List<int>> Weave(int prefix, IEnumerable<List<int>> left, IEnumerable<List<int>> right)
+        {
+            var result = new List<List<int>>();
+
+            foreach (var l in left)
+            {
+                foreach (var r in right)
+                {
+                    var weaved = Weave(new List<int> {prefix}, l, r).ToArray();
+                    result.AddRange(weaved);
+                }
+            }
+
+            return result;
+        }
+
+        private static IEnumerable<List<int>> Weave(List<int> prefix, List<int> left, List<int> right)
+        {
+            if (left.Count == 0 || right.Count == 0)
+            {
+                return new[] {prefix.Concat(left).Concat(right).ToList()};
+            }
+
+            var result = new List<List<int>>();
+
+            var headLeft = left[0];
+            prefix.Add(headLeft);
+            left.RemoveAt(0);
+            result.AddRange(Weave(prefix, left, right).ToList());
+            prefix.RemoveAt(prefix.Count - 1);
+            left.Insert(0, headLeft);
+
+            var headRight = right[0];
+            prefix.Add(headRight);
+            right.RemoveAt(0);
+            result.AddRange(Weave(prefix, left, right).ToList());
+            prefix.RemoveAt(prefix.Count - 1);
+            right.Insert(0, headRight);
+
+            return result;
+        }
+
+        public class TreeNode
+        {
+            public int Value { get; set; }
+            public TreeNode Left { get; set; }
+            public TreeNode Right { get; set; }
+        }
+
+        [TestFixture]
+        public class TreePermutationsTests
+        {
+            [Test]
+            public void Test()
+            {
+                var node = new TreeNode
+                {
+                    Value = 3,
+                    Left = new TreeNode
+                    {
+                        Value = 2,
+                        Left = new TreeNode { Value = 1 },
+                    },
+                    Right = new TreeNode {Value = 4}
+                };
+                var res = ListArrays(node).ToArray();
+                Assert.AreEqual(3, res.Length);
+                CollectionAssert.AreEqual(new[] {3, 2, 1, 4}, res[0]);
+                CollectionAssert.AreEqual(new[] {3, 2, 4, 1}, res[1]);
+                CollectionAssert.AreEqual(new[] {3, 4, 2, 1}, res[2]);
+            }
+        }
+    }
+}
