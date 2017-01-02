6d00688b1178f6d3354a0b64add561c56f4a2c85
Konstantin Startsev <kstartsev@gmail.com>
2017-01-02 18:34:57 +0700

    +

---
+++ CCI/CCI/8.7/StringPermutations.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace CCI
+{
+    public class StringPermutations
+    {
+        public static IEnumerable<string> Find(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                yield return "";
+                yield break;
+            }
+
+            if (s.Length == 1)
+            {
+                yield return s;
+                yield break;
+            }
+
+            var head = s[0];
+            var tail = s.Substring(1);
+            foreach (var str in Find(tail))
+            {
+                for (var i = 0; i <= str.Length; i++)
+                {
+                    yield return str.Substring(0, i) + head + str.Substring(i);
+                }
+            }
+        }
+
+        [TestFixture]
+        public class StringPermutationsTests
+        {
+            [Test]
+            public void Test.1()
+            {
+                CollectionAssert.AreEqual(new[] {"ab", "ba"}, Find("ab"));
+                CollectionAssert.AreEqual(new[] {"abc", "bac", "bca", "acb", "cab", "cba"}, Find("abc"));
+            }
+        }
+    }
+}
---
+++ CCI/CCI/8.9/BracketsPermutations.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace CCI
+{
+    public class BracketsPermutations
+    {
+        public static IEnumerable<string> Find(int n)
+        {
+            return FindImpl(n).Distinct();
+        }
+
+        private static IEnumerable<string> FindImpl(int n)
+        {
+            if (n == 0)
+            {
+                yield return null;
+                yield break;
+            }
+
+            if (n == 1)
+            {
+                yield return "()";
+                yield break;
+            }
+
+            foreach (var str in FindImpl(n - 1))
+            {
+                yield return $"(){str}";
+                yield return $"({str})";
+                yield return $"{str}()";
+            }
+        }
+
+        [TestFixture]
+        public class BracketsPermutationsTests
+        {
+            [Test]
+            public void Test()
+            {
+                CollectionAssert.AreEqual(new[] {"()"}, Find(1));
+                CollectionAssert.AreEqual(new[] {"()()", "(())"}, Find(2));
+                CollectionAssert.AreEqual(new[] {"()()()", "(()())", "()(())", "((()))", "(())()"}, Find(3));
+            }
+        }
+    }
+}
